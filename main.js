a9755f015e7793d3d74aa4a2b684ddde86af292c
jones58 <108466184+jones58@users.noreply.github.com>
2023-10-13 13:42:38 +0100

    editing text and link

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-// getting current date and time //
+function getDateTime() {
+  // getting current date and time //
+  const currentDateTime = new Date().toLocaleString("en-GB", {
+    timeZone: "Europe/London",
+  });
+  // date and time showing on page //
+  const dateOnPage = document.getElementById("date");
+  dateOnPage.textContent = currentDateTime;
+}
 
-const currentDateTime = new Date().toLocaleString("en-GB", {
-  timeZone: "Europe/London",
+document.addEventListener("DOMContentLoaded", function () {
+  getDateTime();
+  setInterval(getDateTime, 1000);
 });
-
-// date and time showing on page //
-
-const dateOnPage = document.getElementById("date");
-dateOnPage.textContent = currentDateTime;
